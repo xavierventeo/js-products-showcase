0d6b061a30f2775f206531b8ff933285266bf3ae
Xavier Venteo <xavierventeo@gmail.com>
2020-05-03 18:55:47 +0200

    Create Products JSON variable

---
+++ data/data.js
@@ -0,0 +1,5 @@
+var productsJSON = `[ 
+    {"id" : "1", "name" : "Mandarina", "description" : "Deliciosa mandarina de la huerta de Valencia", "image" : "img/mandarina.jpg", "prize" : "1,66", "currency" : "€"},
+    {"id" : "2", "name" : "Plátano", "description" : "Plátano de Canarias", "image" : "img/platano.jpg", "prize" : "1,25", "currency" : "€" },
+    {"id" : "3", "name" : "Pera", "description" : "Pera Conference", "image" : "img/pera-conference.jpg", "prize" : "1,30", "currency" : "€" }
+]`;
