2a68435d8e303d71fe221f496bc85bc600f4ce8a
Xavier Venteo <xavierventeo@gmail.com>
2020-05-04 18:30:54 +0200

    Defined manual modal shopping cart

--- js/dragNdrop.js
+++ js/dragNdrop.js
@@ -1,12 +1,4 @@
-function allowDrop(ev) {
-    ev.preventDefault();
-}
-  
-function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-}
-  
-function drop(ev) {
+const getTotalAmountOfShoppingCartAfterEvent = ev => {
     let product_id = ev.dataTransfer.getData("text");
     let prize = document.getElementById("price_" + product_id).innerHTML;
     prize = prize.replace(",", ".");
@@ -16,9 +8,42 @@ function drop(ev) {
 
     let totalAmount = parseFloat(currentAmount) + parseFloat(prize);
     totalAmount = totalAmount.toString().replace(".", ",");
+    return totalAmount;
+}
 
+const generateShoppingCartItem = ev => {
+    let product_id = ev.dataTransfer.getData("text");
+    let prize = document.getElementById("price_" + product_id).innerHTML;
+    prize = prize.replace(",", ".");
+
+    let currentAmount = document.getElementById("total_amount").innerHTML;
+    currentAmount = currentAmount.replace(",", ".");
+
+    let totalAmount = parseFloat(currentAmount) + parseFloat(prize);
+    totalAmount = totalAmount.toString().replace(".", ",");
+    return totalAmount;
+}
+
+const allowDrop = ev => {
+    ev.preventDefault();
+}
+  
+const drag = ev => {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+  
+const drop = ev => {
+    //Intentionally don't transfer the data dragged: the image
+    //If we want to do that: Try with
+    //var data = ev.dataTransfer.getData("text");
+    //ev.target.appendChild(document.getElementById(data));
     ev.preventDefault();
 
+    totalAmount = getTotalAmountOfShoppingCartAfterEvent(ev);
     let divCartUnit = document.getElementById("total_amount");
     divCartUnit.innerHTML = totalAmount;
+
+    let divItemForShoppingCart =  generateShoppingCartItem(ev);
+    let divShoppingCart = document.getElementById("modal-shopping-cart-products");
+    divShoppingCart.innerHTML = divItemForShoppingCart;
 }
