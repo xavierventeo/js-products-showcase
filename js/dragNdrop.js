8aa76fff2f34c0bcadd3f6b28653d5667e205ce7
Xavier Venteo <xavierventeo@gmail.com>
2020-05-27 16:00:59 +0200

    Completed delete item from shopping cart

--- js/dragNdrop.js
+++ js/dragNdrop.js
@@ -22,11 +22,6 @@ const removeItemFromShoppingCart = (elementRemove, productPrice) => {
     row = elementRemove.parentElement;
     row.parentElement.removeChild(row);
 
-
-/*
-
-
-    document.getElementById("modal-shopping-cart-products").deleteRow(elementTR.rowIndex);
     let currentAmount = document.getElementById("total_amount").innerHTML;
 
     productPrice =  productPrice.replace(",", ".");
@@ -37,7 +32,6 @@ const removeItemFromShoppingCart = (elementRemove, productPrice) => {
 
     document.getElementById("total_amount").innerHTML = totalAmount;
     document.getElementById("total-shopping-cart").innerHTML = totalAmount;
-*/
 }
 
 const addItemToShoppingCart = (ev) => {
@@ -85,41 +79,6 @@ const addItemToShoppingCart = (ev) => {
     })
 }
 
-const generateShoppingCartItemAtModal = (ev, totalAmount) => {
-    let product_id = ev.dataTransfer.getData("text");
-    let price = document.getElementById("price_" + product_id).innerHTML;
-    let name = document.getElementById("name_" + product_id).innerHTML;
-    let currency = document.getElementById("currency_" + product_id).innerHTML;
-
-    let table = document.getElementById("modal-shopping-cart-products");
-
-    let row = table.insertRow(0);
-    let cell1 = row.insertCell(0);
-    let cell2 = row.insertCell(1);
-    let cell3 = row.insertCell(2);
-
-    let elementName = document.createElement("span");
-    elementName.setAttribute("class","product-name");
-    elementName.innerHTML = name;
-    cell1.appendChild(elementName);
-
-    let elementPrice = document.createElement("span");
-    elementPrice.setAttribute("class","product-price");
-    elementPrice.innerHTML = price.toString() + currency;
-    cell2.appendChild(elementPrice);
-
-    let elementRemove = document.createElement("span");
-    elementRemove.setAttribute("class","remove");
-    elementRemove.setAttribute("title","Eliminar este producto de mi carrito");
-    elementRemove.setAttribute("onclick",`removeProductFromShoppingCart(this, "${price.toString()}")`);
-
-    elementRemove.innerHTML = "×";
-    cell3.appendChild(elementRemove);
-
-    let itemTotalAmount = document.getElementById("total-shopping-cart");
-    itemTotalAmount.innerHTML = totalAmount + currency;
-}
-
 const allowDrop = ev => {
     ev.preventDefault();
 }
@@ -153,6 +112,5 @@ const drop = ev => {
     let divCartUnit = document.getElementById("total_amount");
     divCartUnit.innerHTML = totalAmount+globalCurrency;
 
-    generateShoppingCartItemAtModal(ev, totalAmount);
     addItemToShoppingCart(ev);
 }
